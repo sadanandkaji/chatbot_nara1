7ad781e76f4948edde9760556837dda7c42f5013
sadanandkaji <sadanandkaji1@gmail.com>
2025-10-27 14:34:14 +0530

    added

--- database.py
+++ database.py
@@ -2,8 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg2://neondb_owner:----------/neondb?sslmode=require"
-
+DATABASE_URL = "postgresql+psycopg2://neondb_owner:npg_QbCMt3KI9xEn@ep-lucky-mode-ahurnzec-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require"
 # Create the SQLAlchemy engine
 engine = create_engine(DATABASE_URL, echo=True)
 
